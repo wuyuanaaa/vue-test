0499f448ce9e91e03237619411324f8b7a468a9f
yuanaaa <dao332508553@163.com>
2018-06-03 20:00:41 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img
+          class="swiper-img"
+          :src="item.imgUrl"
+        >
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://m.ykclass.com/zt/qcbp/1/images/ban-517.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://m.ykclass.com/zt/gwy/4/images/tu428.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color: #fff
+  .wrapper
+    width 100%
+    height: 0
+    overflow hidden
+    padding-bottom 31.25%
+    background-color: #eee
+    .swiper-slide
+      width 100%
+      height: 0
+      overflow hidden
+      padding-bottom 31.25%
+      .swiper-img
+        width 100%
+
+</style>
